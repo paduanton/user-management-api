97233a340677690c0e517e83ac9469c3b4c0df1d
Antonio de Pádua <antonio.junior.h@gmail.com>
2023-03-31 14:39:58 -0300

    refactor: removing unused imports

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { UsersRepository } from './repository/users.repository';
 import { ProfilePhotoRepository } from './repository/profile-photo.repository';
 
-import { UsersService } from './services/users.services';
 import { HttpModule } from '@nestjs/axios';
 
 import { UsersController } from './users.controller';
@@ -28,6 +27,6 @@ import {
     ]),
   ],
   controllers: [UsersController],
-  providers: [UsersRepository, ProfilePhotoRepository, UsersService],
+  providers: [UsersRepository, ProfilePhotoRepository],
 })
 export class UsersModule {}
